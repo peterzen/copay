c93e31dd3a45ebe4823860ce92ecd9fa8a6e9263
Peter Banik <peter@froggle.org>
2017-05-15 15:18:46 +0200

    Payment protocol URI scheme changed to decred:...

--- src/js/services/openURL.js
+++ src/js/services/openURL.js
@@ -57,10 +57,10 @@ angular.module('copayApp.services').factory('openURLService', function($rootScop
 
       // This event is sent to an existent instance of Copay (only for standalone apps)
       gui.App.on('open', function(pathData) {
-        if (pathData.indexOf('bitcoin:') != -1) {
+        if (pathData.indexOf('decred:') != -1) {
           $log.debug('Bitcoin URL found');
           handleOpenURL({
-            url: pathData.substring(pathData.indexOf('bitcoin:'))
+            url: pathData.substring(pathData.indexOf('decred:'))
           });
         } else if (pathData.indexOf(appConfigService.name + '://') != -1) {
           $log.debug(appConfigService.name + ' URL found');
