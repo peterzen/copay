bd4a4ffca554a89e95aec7f36820a10e2f492c99
Peter Banik <peter@froggle.org>
2017-05-15 15:00:05 +0200

    Backend URLs points to decred.org

--- src/js/services/configService.js
+++ src/js/services/configService.js
@@ -12,7 +12,7 @@ angular.module('copayApp.services').factory('configService', function(storageSer
 
     // Bitcore wallet service URL
     bws: {
-      url: 'https://bws.bitpay.com/bws/api',
+      url: 'https://bws.decred.org/bws/api',
     },
 
     download: {
