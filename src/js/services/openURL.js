78b5f85710365900b6716de2da4d859a1f166942
Peter Banik <peter@froggle.org>
2017-05-16 03:50:37 +0200

    URL handler registered to handle decred: links

--- src/js/services/openURL.js
+++ src/js/services/openURL.js
@@ -83,9 +83,7 @@ angular.module('copayApp.services').factory('openURLService', function($rootScop
 
       if (navigator.registerProtocolHandler) {
         $log.debug('Registering Browser handlers base:' + base);
-        navigator.registerProtocolHandler('bitcoin', url, 'Copay Bitcoin Handler');
-        navigator.registerProtocolHandler('web+copay', url, 'Copay Wallet Handler');
-        navigator.registerProtocolHandler('web+bitpay', url, 'Bitpay Wallet Handler');
+        navigator.registerProtocolHandler('decred', url, 'Decred Wallet Handler');
       }
     }
   };
