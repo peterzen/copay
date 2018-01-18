b9f695e7fdec0d5dbeed3c44c7da3fc02d0ad892
Peter Banik <peter@froggle.org>
2018-01-18 21:39:23 +0100

    Build script creates serve image as well.

--- build_docker.sh
+++ build_docker.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
-docker build -t decred/copay-builder -f Dockerfiles/Dockerfile-builder . \
-&& docker run --rm -v `pwd`/dist:/dist decred/copay-builder
+CUR_DIR=$(pwd)
+SERVE_IMAGE=decred/webwallet
 
+rm -rf dist && mkdir dist && \
+docker build -t decred/copay-builder -f Dockerfiles/Dockerfile-builder . && \
+docker run --rm -v $CUR_DIR/dist:/dist decred/copay-builder && \
+docker build -f Dockerfiles/Dockerfile-production -t SERVE_IMAGE &&  \
+echo Docker image $SERVE_IMAGE created
