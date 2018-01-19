6905e44b41262d9b752b0dfe96e72fd888353eed
Peter Banik <peter@froggle.org>
2018-01-19 18:23:29 +0100

    Dockerized build script (#76)
    * Dockerized build script
    
    Added builld script, fixed the bower dependencies that broke the build
    
    * Build script creates serve image as well.

---
+++ build_docker.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+CUR_DIR=$(pwd)
+SERVE_IMAGE=decred/webwallet
+
+rm -rf dist && mkdir dist && \
+docker build -t decred/copay-builder -f Dockerfiles/Dockerfile-builder . && \
+docker run --rm -v $CUR_DIR/dist:/dist decred/copay-builder && \
+docker build -f Dockerfiles/Dockerfile-production -t SERVE_IMAGE &&  \
+echo Docker image $SERVE_IMAGE created
