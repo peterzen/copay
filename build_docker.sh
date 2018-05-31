a12304e3d25a8bd32d3b3f362d9d4e46c6fd689c
Peter Banik <peter@froggle.org>
2018-05-31 15:32:42 +0200

    Auto stash before rebase of "origin/master"

--- build_docker.sh
+++ build_docker.sh
@@ -6,5 +6,5 @@ SERVE_IMAGE=decred/webwallet
 rm -rf dist && mkdir dist && \
 docker build -t decred/copay-builder -f Dockerfiles/Dockerfile-builder . && \
 docker run --rm -v $CUR_DIR/dist:/dist decred/copay-builder && \
-docker build -f Dockerfiles/Dockerfile-production -t SERVE_IMAGE &&  \
+docker build -f Dockerfiles/Dockerfile-production -t $SERVE_IMAGE . &&  \
 echo Docker image $SERVE_IMAGE created
